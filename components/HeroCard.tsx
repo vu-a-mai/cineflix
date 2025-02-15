1f3c1c74d5a27f6c0ec01793a2aa539d71d6d0f0
Vu Mai <vuanhmai.io@gmail.com>
2025-02-15 08:54:51 -0800

    - Updated HeroCard to display Modal component

--- components/HeroCard.tsx
+++ components/HeroCard.tsx
@@ -1,52 +1,67 @@
+"use client";
+
 import { baseImgUrl } from "@lib/constants";
 import { Movie } from "@lib/types";
 import {
   InfoOutlined,
   PlayCircleOutlineOutlined,
 } from "@node_modules/@mui/icons-material";
+import { useState } from "react";
+import Modal from "./Modal";
 
 // HeroCard component will display the movie
 const HeroCard = ({ trendingMovie }: { trendingMovie: Movie }) => {
   // Log the trending movie
   //console.log(trendingMovie);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
   return (
-    <div className="hero">
-      {/* hero background */}
-      <div className="hero-bg">
-        {/* image */}
-        <img
-          src={`${baseImgUrl}${
-            trendingMovie?.backdrop_path || trendingMovie?.poster_path
-          }`}
-          alt="trending-movie"
-          className="hero-bg-image"
-        />
+    <>
+      <div className="hero">
+        {/* hero background */}
+        <div className="hero-bg">
+          {/* image */}
+          <img
+            src={`${baseImgUrl}${
+              trendingMovie?.backdrop_path || trendingMovie?.poster_path
+            }`}
+            alt="trending-movie"
+            className="hero-bg-image"
+          />
 
-        {/* overlay */}
-        <div className="overlay"></div>
+          {/* overlay */}
+          <div className="overlay"></div>
+        </div>
+
+        {/* title */}
+        <h1 className="hero-title">
+          {trendingMovie?.title || trendingMovie?.name}
+        </h1>
+
+        {/* overview */}
+        <p className="hero-overview">{trendingMovie?.overview}</p>
+
+        {/* buttons */}
+        <div className="hero-btns">
+          {/* play now */}
+          <button className="hero-btn" onClick={openModal}>
+            <PlayCircleOutlineOutlined /> Play Now
+          </button>
+
+          {/* more info */}
+          <button className="hero-btn" onClick={openModal}>
+            <InfoOutlined /> More Info
+          </button>
+        </div>
       </div>
 
-      {/* title */}
-      <h1 className="hero-title">
-        {trendingMovie?.title || trendingMovie?.name}
-      </h1>
-
-      {/* overview */}
-      <p className="hero-overview">{trendingMovie?.overview}</p>
-
-      {/* buttons */}
-      <div className="hero-btns">
-        {/* play now */}
-        <button className="hero-btn">
-          <PlayCircleOutlineOutlined /> Play Now
-        </button>
-
-        {/* more info */}
-        <button className="hero-btn">
-          <InfoOutlined /> More Info
-        </button>
-      </div>
-    </div>
+      {/* Modal */}
+      {showModal && <Modal movie={trendingMovie} closeModal={closeModal} />}
+    </>
   );
 };
 
