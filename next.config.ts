c1a300b876d52e122091aad9fbacc05c7a709afd
Vu Mai <vuanhmai.io@gmail.com>
2025-02-22 22:18:28 -0800

    - Updated nextConfig due to vercel deploying error ( Build error occurred Error [ValidationError]: Invalid configuration object. Webpack has been initialized using a configuration object that does not match the API schema. - configuration.cache.type should be "memory".)

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,11 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config, { dev }) => {
-    // Set cache type based on environment
-    config.cache = dev
-      ? {
-          type: "memory", // Use memory cache in development
-        }
-      : {
-          type: "filesystem", // Use filesystem cache in production
-          buildDependencies: {
-            config: [__filename],
-          },
-          cacheLocation: path.resolve(__dirname, "node_modules/.cache/webpack"),
-          store: "pack",
-          packOptions: {
-            serialization: {
-              useBuffer: true, // Optimize serialization
-            },
-          },
-        };
-
+  webpack: (config) => {
+    // Set cache type to memory for all environments
+    config.cache = {
+      type: "memory",
+    };
     return config; // Return the modified Webpack config
   },
 };
